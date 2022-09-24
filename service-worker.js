de98bc98a45e82bfea77cc9bbdcc0c19dd689e93
Jêfferson Maxwell <jeffersonmaxwell1603@gmail.com>
2022-09-24 13:21:44 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Comentarios-Interativos/precache-manifest.eeeec7f14448c6e4799781d2a343b881.js"
+  "./precache-manifest.95b80633a18334a56189ccf5af120c8c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Comentarios-Interativos/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
